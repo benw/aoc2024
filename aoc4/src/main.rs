8d669ff8c3f7ece3290f3dd3887969b4027b09b3
Ben Williamson <benw@pobox.com>
2024-12-04 19:00:29 +1100

    Day 4 part 1

---
+++ aoc4/src/main.rs
@@ -0,0 +1,61 @@
+
+struct Grid {
+    rows: [[u8; 140]; 140],
+}
+
+impl Grid {
+    fn new() -> Self {
+        Grid { rows: [[0; 140]; 140] }
+    }
+
+    fn get_char(&self, x: i32, y: i32) -> u8 {
+        if x >= 0 && x < 140 && y >=0 && y < 140 {
+            self.rows[y as usize][x as usize]
+        } else {
+            0
+        }
+    }
+
+    fn get_word(&self, x: i32, y: i32, dx: i32, dy: i32) -> [u8; 4] {
+        [
+            self.get_char(x + dx * 0, y + dy * 0),
+            self.get_char(x + dx * 1, y + dy * 1),
+            self.get_char(x + dx * 2, y + dy * 2),
+            self.get_char(x + dx * 3, y + dy * 3),
+        ]
+    }
+}
+
+fn test_word(word: [u8; 4]) -> u32 {
+    if &word == b"XMAS" {
+        1
+    } else {
+        0
+    }
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    let mut grid = Grid::new();
+    for (y, line) in input.lines().enumerate() {
+        for (x, ch) in line.chars().enumerate() {
+            grid.rows[y][x] = ch as u8;
+        }
+    }
+
+    let mut count = 0;
+    for y in 0..140 {
+        for x in 0..140 {
+            count += test_word(grid.get_word(x, y, 1, 0));
+            count += test_word(grid.get_word(x, y, 1, 1));
+            count += test_word(grid.get_word(x, y, 0, 1));
+            count += test_word(grid.get_word(x, y, -1, 1));
+            count += test_word(grid.get_word(x, y, -1, 0));
+            count += test_word(grid.get_word(x, y, -1, -1));
+            count += test_word(grid.get_word(x, y, 0, -1));
+            count += test_word(grid.get_word(x, y, 1, -1));
+        }
+    }
+    println!("Part 1: {}", count);
+}
