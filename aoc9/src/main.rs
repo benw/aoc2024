ce4e7d223c97867585d6a1b0d06e0ce111de09e0
Ben Williamson <benw@pobox.com>
2024-12-10 14:40:16 +1100

    Day 9 part 2

--- aoc9/src/main.rs
+++ aoc9/src/main.rs
@@ -2,31 +2,46 @@ fn main() {
     let input = include_str!("input.txt");
     //let input = "2333133121414131402";
 
-    let mut blocks = spread(input);
+    let (mut blocks, files) = spread(input);
+    let mut blocks2 = blocks.clone();
     compact(&mut blocks);
-    let checksum = checksum(&blocks);
-    println!("Part 1: {}", checksum);
+    let sum = checksum(&blocks);
+    println!("Part 1: {}", sum);
+
+    compact2(&mut blocks2, &files);
+    let sum = checksum(&blocks2);
+    println!("Part 2: {}", sum);
 }
 
-fn spread(input: &str) -> Vec<Option<u32>> {
+#[derive(Debug)]
+struct File {
+    id: u32,
+    index: usize,
+    len: usize,
+}
+
+// (blocks = Vec<Option<id>>, files = Vec<File> in id order)
+fn spread(input: &str) -> (Vec<Option<u32>>, Vec<File>) {
     let mut it = input.chars();
     let mut blocks = vec![];
+    let mut files = vec![];
     let mut id = 0;
     loop {
         let Some(ch) = it.next() else {
             break;
         };
-        let count = ch.to_digit(10).unwrap() as usize;
-        blocks.extend(std::iter::repeat_n(Some(id), count));
+        let len = ch.to_digit(10).unwrap() as usize;
+        files.push(File { id, index: blocks.len(), len });
+        blocks.extend(std::iter::repeat_n(Some(id), len));
         id += 1;
 
         let Some(ch) = it.next() else {
             break;
         };
-        let count = ch.to_digit(10).unwrap() as usize;
-        blocks.extend(std::iter::repeat_n(None, count));
+        let len = ch.to_digit(10).unwrap() as usize;
+        blocks.extend(std::iter::repeat_n(None, len));
     }
-    blocks
+    (blocks, files)
 }
 
 fn compact(blocks: &mut Vec<Option<u32>>) {
@@ -40,10 +55,40 @@ fn compact(blocks: &mut Vec<Option<u32>>) {
     }
 }
 
+fn compact2(blocks: &mut Vec<Option<u32>>, files: &[File]) {
+    for file in files.iter().rev() {
+        if let Some(index) = find_free(&blocks[..file.index], file.len) {
+            blocks[index..index+file.len].fill(Some(file.id));
+            blocks[file.index..file.index+file.len].fill(None);
+            //print_blocks(blocks);
+        }
+
+    }
+}
+
+fn find_free(blocks: &[Option<u32>], len: usize) -> Option<usize> {
+    for i in 0..blocks.len() {
+        if i+len <= blocks.len() && blocks[i..i+len].iter().all(|block| block.is_none()) {
+            return Some(i);
+        }
+    }
+    None
+}
+
 fn checksum(blocks: &[Option<u32>]) -> u64 {
     let mut total = 0;
     for (i, b) in blocks.iter().enumerate() {
-        total += (i as u64) * (*b.as_ref().unwrap() as u64) ;
+        total += (i as u64) * (*b.as_ref().unwrap_or(&0) as u64) ;
     }
     total
+}
+
+fn print_blocks(blocks: &[Option<u32>]) {
+    for block in blocks {
+        match block {
+            Some(id) => print!("{}", id),
+            None => print!("."),
+        }
+    }
+    println!("");
 }
