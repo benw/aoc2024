f95f26042e72e091343504225f22240e7c294127
Ben Williamson <benw@pobox.com>
2024-12-14 21:48:20 +1100

    Day 11 part 2 wip

--- aoc11/src/main.rs
+++ aoc11/src/main.rs
@@ -2,33 +2,32 @@ fn main() {
     let input = [965842, 9159, 3372473, 311, 0, 6, 86213, 48];
     //let input = [125, 17];
 
-    let mut v = Vec::from(&input);
-    for i in 0..25 {
-        let out = blink(&v);
-        v = out;
-        //println!("{}: {:?}", i, v);
-        println!("{}: {}", i, v.len());
-    }
-    println!("Part 1: {}", v.len());
+    println!("Part 1: {}", stones(&input, 25));
+    println!("Part 2: {}", stones(&input, 75));
 }
 
-fn blink(input: &[u64]) -> Vec<u64> {
-    let mut out = vec![];
+fn stones(input: &[u64], blinks: u32) -> u32 {
+    let mut count = 0;
     for &i in input {
-        if i == 0 {
-            out.push(1);
+        count += f(i, blinks);
+    }
+    count
+}
+
+fn f(i: u64, blinks: u32) -> u32 {
+    if blinks == 0 {
+        1
+    } else if i == 0 {
+        f(1, blinks - 1)
+    } else {
+        let dig = digits(i);
+        if dig % 2 == 0 {
+            let (left, right) = split(i, dig / 2);
+            f(left, blinks - 1) + f(right, blinks - 1)
         } else {
-            let dig = digits(i);
-            if dig % 2 == 0 {
-                let (left, right) = split(i, dig / 2);
-                out.push(left);
-                out.push(right);
-            } else {
-                out.push(i * 2024);
-            }
+            f(i * 2024, blinks - 1)
         }
     }
-    out
 }
 
 fn digits(mut i: u64) -> u32 {
