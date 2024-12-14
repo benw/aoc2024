27861ae6759c45f84483618e8cdd558ea4ee986e
Ben Williamson <benw@pobox.com>
2024-12-14 23:31:49 +1100

    Day 12 part 2

--- aoc12/src/main.rs
+++ aoc12/src/main.rs
@@ -4,8 +4,9 @@ fn main() {
     let input = include_str!("input.txt");
 
     let grid = Grid::new(input);
-    println!("Part 1: {}", grid.cost());
-
+    let (a, b) = grid.cost();
+    println!("Part 1: {}", a);
+    println!("Part 2: {}", b);
 }
 
 #[derive(Debug, Clone)]
@@ -30,38 +31,68 @@ impl Grid {
         0 as char
     }
 
-    fn cost(mut self) -> u64 {
+    fn cost(mut self) -> (u64, u64) {
         let mut total = 0;
+        let mut total_part2 = 0;
         for y in 0..self.rows.len() {
             for x in 0..self.rows[y].len() {
                 let c = self.rows[y][x];
                 if c != '.' {
                     let mut perim = 0;
                     let mut set = HashSet::new();
-                    self.fill(x as i32, y as i32, c, &mut perim, &mut set);
+                    let mut fence = [HashSet::new(), HashSet::new(), HashSet::new(), HashSet::new()];
+                    self.fill(x as i32, y as i32, c, &mut perim, &mut set, &mut fence);
                     total += set.len() as u64 * perim;
+                    total_part2 += set.len() as u64 * sides(fence);
                     for (xx, yy) in set {
                         self.rows[yy as usize][xx as usize] = '.';
                     }
                 }
             }
         }
-        total
+        (total, total_part2)
     }
 
-    fn fill(&mut self, x: i32, y: i32, c: char, perim: &mut u64, set: &mut HashSet<(i32, i32)>) {
+    fn fill(&mut self, x: i32, y: i32, c: char, perim: &mut u64, set: &mut HashSet<(i32, i32)>, fence: &mut [HashSet<(i32, i32)>; 4]) {
         if set.contains(&(x, y)) {
             return;
         }
         set.insert((x, y));
-        for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+        for (dir, (dx, dy)) in [(1, 0), (-1, 0), (0, 1), (0, -1)].into_iter().enumerate() {
             if !set.contains(&(x + dx, y + dy)) {
                 if self.get(x + dx, y + dy) == c {
-                    self.fill(x + dx, y + dy, c, perim, set);
+                    self.fill(x + dx, y + dy, c, perim, set, fence);
                 } else {
                     *perim += 1;
+                    fence[dir].insert((x, y));
                 }
             }
         }
     }
+}
+
+fn sides(mut fence: [HashSet<(i32, i32)>; 4]) -> u64 {
+    let mut sides = 0;
+    for (dir, (dx, dy)) in [(1, 0), (-1, 0), (0, 1), (0, -1)].into_iter().enumerate() {
+        let f = &mut fence[dir];
+        while f.len() > 0 {
+            sides += 1;
+            let &(x, y) = f.iter().next().unwrap();
+            let mut xx = x;
+            let mut yy = y;
+            while f.remove(&(xx, yy)) {
+                xx += dy;
+                yy += dx;
+            }
+            let mut xx = x;
+            let mut yy = y;
+            xx -= dy;
+            yy -= dx;
+        while f.remove(&(xx, yy)) {
+                xx -= dy;
+                yy -= dx;
+            }
+        }
+    }
+    sides
 }
