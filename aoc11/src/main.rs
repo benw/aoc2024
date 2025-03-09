727f3d551086af4b780f4f8c2ac1c0799eeda6d4
Ben Williamson <12386+benw@users.noreply.github.com>
2025-03-09 21:23:29 +1100

    Day 11 part 2

--- aoc11/src/main.rs
+++ aoc11/src/main.rs
@@ -1,33 +1,41 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = [965842, 9159, 3372473, 311, 0, 6, 86213, 48];
     //let input = [125, 17];
 
-    println!("Part 1: {}", stones(&input, 25));
-    println!("Part 2: {}", stones(&input, 75));
+    let mut memo = HashMap::new();
+    println!("Part 1: {}", stones(&input, 25, &mut memo));
+    println!("Part 2: {}", stones(&input, 75, &mut memo));
 }
 
-fn stones(input: &[u64], blinks: u32) -> u32 {
+fn stones(input: &[u64], blinks: u64, memo: &mut HashMap<(u64, u64), u64>) -> u64 {
     let mut count = 0;
     for &i in input {
-        count += f(i, blinks);
+        count += f(i, blinks, memo);
     }
     count
 }
 
-fn f(i: u64, blinks: u32) -> u32 {
-    if blinks == 0 {
+fn f(i: u64, blinks: u64, memo: &mut HashMap<(u64, u64), u64>) -> u64 {
+    if let Some(res) = memo.get(&(i, blinks)) {
+        return *res;
+    }
+    let res = if blinks == 0 {
         1
     } else if i == 0 {
-        f(1, blinks - 1)
+        f(1, blinks - 1, memo)
     } else {
         let dig = digits(i);
         if dig % 2 == 0 {
             let (left, right) = split(i, dig / 2);
-            f(left, blinks - 1) + f(right, blinks - 1)
+            f(left, blinks - 1, memo) + f(right, blinks - 1, memo)
         } else {
-            f(i * 2024, blinks - 1)
+            f(i * 2024, blinks - 1, memo)
         }
-    }
+    };
+    memo.insert((i, blinks), res);
+    res
 }
 
 fn digits(mut i: u64) -> u32 {
