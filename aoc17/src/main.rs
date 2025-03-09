09467e0d58c0d94f23d7b3f884c03922e177737a
Ben Williamson <12386+benw@users.noreply.github.com>
2025-03-09 21:23:30 +1100

    Day 17 part 2

--- aoc17/src/main.rs
+++ aoc17/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+use std::fmt::Display;
 
 
 #[derive(Copy, Clone, Default)]
@@ -85,6 +87,41 @@ impl Cpu {
     }
 }
 
+fn outputs_self(a: u64, mem: &[u8]) -> bool {
+    let mut cpu: Cpu = Default::default();
+    cpu.a = a;
+    let mut good = true;
+    for &x in mem {
+        good = match cpu.run_to_out(&mem) {
+            None => false,
+            Some(out) => out == x,
+        };
+        if !good {
+            break;
+        }
+    }
+    good && cpu.run_to_out(&mem).is_none()
+}
+
+struct CommaSep<I>(I);
+
+impl <T> Display for CommaSep<T>
+where
+    T: Clone + IntoIterator,
+    T::Item: Display,
+{
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let mut it = self.0.clone().into_iter();
+        if let Some(first) = it.next() {
+            write!(fmt, "{}", first)?;
+        }
+        while let Some(item) = it.next() {
+            write!(fmt, ",{}", item)?;
+        }
+        Ok(())
+    }
+}
+
 fn main() {
     let mut cpu = Cpu {
         a: 47719761,
@@ -94,32 +131,60 @@ fn main() {
     };
     let mem = [2,4,1,5,7,5,0,3,4,1,1,6,5,5,3,0];
     let out = cpu.run(&mem);
-    print!("Part 1: {}", out[0]);
-    for x in &out[1..] {
-        print!(",{}", x);
+    println!("Part 1, with cpu.run(): {}", CommaSep(&out));
+
+    print!(  "Part 1, with step():    ");
+    let mut a = 47719761;
+    while a != 0 {
+        let (x, new_a) = step(a);
+        print!("{},", x);
+        a = new_a;
     }
     println!("");
 
-    for a in 0.. {
-        if a % 1000000 == 0 {
-            print!("a={}\r", a);
-        }
-        let mut cpu: Cpu = Default::default();
-        cpu.a = a;
-        let mut good = true;
-        for &x in &mem {
-            good = match cpu.run_to_out(&mem) {
-                None => false,
-                Some(out) => out == x,
-            };
-            if !good {
-                break;
-            }
-        }
-        if !good || cpu.run_to_out(&mem).is_some() {
-            continue;
-        }
+    let mut found = vec![];
+    search(&mem, 0, &mut found);
+
+    for &a in &found {
+        assert!(outputs_self(a, &mem));
         println!("Part 2: {}", a);
-        break;
     }
+}
+
+fn search(out: &[u8], a: u64, found: &mut Vec<u64>) {
+    if out.len() == 0 {
+        found.push(a);
+        return;
+    }
+    let len = out.len() - 1;
+    let x = out[len];
+    let rest = &out[..len];
+
+    //println!("Searching for a that results in (out {}, a=0o{:o})", x, a);
+    for a_bits in 0..=7 {
+        let candidate_a = (a << 3) ^ a_bits;
+        if step(candidate_a) == (x, a) {
+            search(rest, candidate_a, found);
+        }
+    }
+}
+
+/*
+2,4,    b = a & 7   dead: b, c
+1,5,    b = b ^ 5
+7,5,    c = a >> b
+0,3,    a = a >> 3
+4,1,    b = b ^ c
+1,6,    b = b ^ 6
+5,5,    out b & 7
+3,0     jnz a, 0
+*/
+fn step(mut a: u64) -> (u8, u64) {
+    let mut b = a & 7;
+    b = b ^ 5;
+    let c = a >> b;
+    a = a >> 3;
+    b = b ^ c;
+    b = b ^ 6;
+    ((b & 7) as u8, a)
 }
