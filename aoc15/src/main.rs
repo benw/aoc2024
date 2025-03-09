1114905665bbc66a11ee0366ff7a58d3c60a2ad5
Ben Williamson <12386+benw@users.noreply.github.com>
2025-03-09 21:23:29 +1100

    Day 15 part 2

--- aoc15/src/main.rs
+++ aoc15/src/main.rs
@@ -39,25 +39,69 @@ peg::parser!{
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Grid {
     rows: Vec<Vec<char>>,
     robot: Vect,
 }
 
 impl Grid {
-    fn try_move(&mut self, pos: Vect, dir: Vect) -> bool {
+    fn can_move(&self, pos: Vect, dir: Vect) -> bool {
         let dest = pos + dir;
-        let able = match self.rows[dest.y as usize][dest.x as usize] {
+        match self.rows[dest.y as usize][dest.x as usize] {
             '.' => true,
             '#' => false,
-            'O' => self.try_move(dest, dir),
+            'O' => self.can_move(dest, dir),
+            '[' => {
+                if dir.x == 0 {
+                    self.can_move(dest, dir) && self.can_move(Vect { x: dest.x + 1, y: dest.y }, dir)
+                } else {
+                    self.can_move(dest, dir)
+                }
+            }
+            ']' => {
+                if dir.x == 0 {
+                    self.can_move(dest, dir) && self.can_move(Vect { x: dest.x - 1, y: dest.y }, dir)
+                } else {
+                    self.can_move(dest, dir)
+                }
+            }
             c => panic!("Unexpected '{}' at {},{}", c, dest.x, dest.y),
-        };
+        }
+    }
+
+    fn do_move(&mut self, pos: Vect, dir: Vect) {
+        let dest = pos + dir;
+        match self.rows[dest.y as usize][dest.x as usize] {
+            '.' => (),
+            '#' => panic!("Cannot move #"),
+            'O' => self.do_move(dest, dir),
+            '[' => {
+                if dir.x == 0 {
+                    self.do_move(dest, dir);
+                    self.do_move(Vect { x: dest.x + 1, y: dest.y }, dir);
+                } else {
+                    self.do_move(dest, dir);
+                }
+            }
+            ']' => {
+                if dir.x == 0 {
+                    self.do_move(dest, dir);
+                    self.do_move(Vect { x: dest.x - 1, y: dest.y }, dir);
+                } else {
+                    self.do_move(dest, dir);
+                }
+            }
+            c => panic!("Unexpected '{}' at {},{}", c, dest.x, dest.y),
+        }
+        assert_eq!('.', self.rows[dest.y as usize][dest.x as usize]);
+        self.rows[dest.y as usize][dest.x as usize] = self.rows[pos.y as usize][pos.x as usize];
+        self.rows[pos.y as usize][pos.x as usize] = '.';
+    }
+    fn try_move(&mut self, pos: Vect, dir: Vect) -> bool {
+        let able = self.can_move(pos, dir);
         if able {
-            assert_eq!('.', self.rows[dest.y as usize][dest.x as usize]);
-            self.rows[dest.y as usize][dest.x as usize] = self.rows[pos.y as usize][pos.x as usize];
-            self.rows[pos.y as usize][pos.x as usize] = '.';
+            self.do_move(pos, dir);
         }
         able
     }
@@ -68,7 +112,7 @@ impl Grid {
         }
     }
 
-    fn display(&self) {
+    fn _display(&self) {
         for row in self.rows.iter() {
             for c in row {
                 print!("{}", c);
@@ -81,14 +125,44 @@ impl Grid {
         let mut total = 0;
         for (y, row) in self.rows.iter().enumerate() {
             for (x, &c) in row.iter().enumerate() {
-                if c == 'O' {
+                if c == 'O' || c == '[' {
                     total += 100 * y + x;
                 }
             }
         }
         total
     }
+
+    fn double(&mut self) {
+        for row in self.rows.iter_mut() {
+            let doubled = double_row(row);
+            let _ = std::mem::replace(row, doubled);
+        }
+        self.robot.x *= 2;
+    }
 }
+
+fn double_row(row: &[char]) -> Vec<char> {
+    let mut doubled = vec![];
+    for &c in row {
+        match c {
+            'O' => {
+                doubled.push('[');
+                doubled.push(']');
+            }
+            '@' => {
+                doubled.push('@');
+                doubled.push('.');
+            }
+            _ => {
+                doubled.push(c);
+                doubled.push(c);
+            }
+        }
+    }
+    doubled
+}
+
 
 fn main() {
     let input = include_str!("input.txt");
@@ -103,9 +177,16 @@ fn main() {
         }
     }
     let mut grid = Grid { rows, robot };
+    let orig_grid = grid.clone();
     for &dir in moves.iter() {
         grid.move_robot(dir);
     }
-    grid.display();
     println!("Part 1: {}", grid.sum_gps());
+
+    let mut grid = orig_grid;
+    grid.double();
+    for &dir in moves.iter() {
+        grid.move_robot(dir);
+    }
+    println!("Part 2: {}", grid.sum_gps());
 }
