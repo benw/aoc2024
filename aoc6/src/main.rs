eeb7321e3c03e9b60a9aa8642e5602f611b4a53d
Ben Williamson <12386+benw@users.noreply.github.com>
2025-03-09 21:23:29 +1100

    Refactor

--- aoc6/src/main.rs
+++ aoc6/src/main.rs
@@ -1,13 +1,24 @@
 const W: usize = 130;
 const H: usize = 130;
 
+#[derive(Copy, Clone)]
 struct Grid {
     rows: [[u8; W]; H],
+    gx: i32,
+    gy: i32,
+    dx: i32,
+    dy: i32,
 }
 
 impl Grid {
     fn new() -> Self {
-        Grid { rows: [[0; W]; H] }
+        Grid {
+            rows: [[0; W]; H],
+            gx: 0,
+            gy: 0,
+            dx: 0,
+            dy: -1,
+        }
     }
 
     fn get_char(&self, x: i32, y: i32) -> u8 {
@@ -17,49 +28,46 @@ impl Grid {
             0
         }
     }
+
+    fn walk_terminates(&mut self) -> bool {
+        loop {
+            self.rows[self.gy as usize][self.gx as usize] = b'X';
+            let next_gx = self.gx + self.dx;
+            let next_gy = self.gy + self.dy;
+            let ch = self.get_char(next_gx, next_gy);
+            match ch {
+                b'.' | b'X' => {
+                    self.gx += self.dx;
+                    self.gy += self.dy;
+                }
+                b'#' | b'O' => {
+                    // turn right
+                    let old_dx = self.dx;
+                    self.dx = -self.dy;
+                    self.dy = old_dx;
+                }
+                0 => break true,
+                _ => panic!("unexpected char '{}'", ch as char),
+            }
+        }
+    }
 }
 
 fn main() {
     let input = include_str!("input.txt");
     //let input = include_str!("sample.txt");
 
-    let mut gx = 0;
-    let mut gy = 0;
-
     let mut grid = Grid::new();
     for (y, line) in input.lines().enumerate() {
         for (x, ch) in line.chars().enumerate() {
             grid.rows[y][x] = ch as u8;
             if ch == '^' {
-                gx = x as i32;
-                gy = y as i32;
+                grid.gx = x as i32;
+                grid.gy = y as i32;
             }
         }
     }
-
-    let mut dx = 0;
-    let mut dy = -1;
-    loop {
-        grid.rows[gy as usize][gx as usize] = b'X';
-        let next_gx = gx + dx;
-        let next_gy = gy + dy;
-        let ch = grid.get_char(next_gx, next_gy);
-        match ch {
-            b'.' | b'X' => {                
-                gx += dx;
-                gy += dy;
-            }
-            b'#' => {
-                // turn right
-                let old_dx = dx;
-                dx = -dy;
-                dy = old_dx;
-            }
-            0 => break,
-            _ => panic!("unexpected char '{}'", ch as char),
-        }
-
-    }
+    assert!(grid.walk_terminates());
 
     let mut count = 0;
     for row in &grid.rows {
